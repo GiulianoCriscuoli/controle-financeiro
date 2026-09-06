bcd75d7b20f6a13d5472c24b19232b2359353692
GiulianoCriscuoli <giulianodias3@hotmail.com>
2026-09-06 15:43:48 -0300

    fix:ajuste de interfaceuser e type account

--- app/Interfaces/Auth/UserInterface.php
+++ app/Interfaces/Auth/UserInterface.php
@@ -7,6 +7,6 @@ use Illuminate\Contracts\Auth\Authenticatable;
 interface UserInterface
 {
     public function login(array $account): Authenticatable;
-    // public function logout(Authenticatable $user): void;
+    public function logout(Authenticatable $user): void;
     public function generateToken(Authenticatable $user): string;
 }
--- app/Interfaces/TypeAccount/TypeAccountInterface.php
+++ app/Interfaces/TypeAccount/TypeAccountInterface.php
@@ -2,7 +2,17 @@
 
 namespace App\Interfaces\TypeAccount;
 
+use App\Models\TypeAccount;
+
 interface TypeAccountInterface
 {
+    public function all(): object;
 
+    public function store(array $data, int $userId): object;
+
+    public function update(int $typeAccountId, array $data): TypeAccount;
+
+    public function destroy(int $typeAccountId): void;
+
+    public function show(int $typeAccountId): TypeAccount;
 }
